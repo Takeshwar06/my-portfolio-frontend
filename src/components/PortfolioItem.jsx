2e63efee01a5d65d4244cf96a85ae4f03dcaffc2
takeshwar06 <takeshwarverma124@gmail.com>
2024-11-26 02:24:12 +0530

    add portfolio,update resume

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,13 +1,97 @@
 import { Link } from "react-router-dom";
 
-export default function PortfolioItem({item}) {
-    return (
-        <div className="portfolio-item padd-15">
-            <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                    <Link to={`${item.url}`} target="_blank"> <img src={`${item.img}`} alt="" srcset="" /></Link>
-                </div>
-            </div>
+export default function PortfolioItem({ item }) {
+  return (
+    <div className="portfolio-item padd-15">
+      <div className="portfolio-item-inner shadow-dark">
+        <div className="portfolio-img" style={{ position: "relative" }}>
+          <img src={`${item.img}`} alt="" />
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "space-between",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              padding: "10px",
+            }}
+          >
+            <Link
+              to={item.githubUrl}
+              target="_blank"
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
+                color: "#fff",
+                fontSize: "10px", // Smaller font size
+                padding: "3px 8px", // Smaller padding
+                border: "1px solid #28a745", // Green border for GitHub
+                borderRadius: "5px",
+                textDecoration: "none",
+                transition: "all 0.3s ease",
+              }}
+              onMouseEnter={(e) => {
+                e.target.style.color = "#fff";
+                e.target.style.backgroundColor = "green";
+                e.target.style.transform = "scale(1.1)";
+              }}
+              onMouseLeave={(e) => {
+                e.target.style.color = "#fff";
+                e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+                e.target.style.transform = "scale(1)";
+              }}
+            >
+              <i
+                className="fa fa-brands fa-github"
+                style={{ marginRight: "5px" }}
+              ></i>
+              Code
+            </Link>
+
+            <Link
+              to={
+                item.download
+                  ? `${window.location.origin}${item.liveUrl}`
+                  : item.liveUrl
+              }
+              target="_blank"
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
+                color: "#fff",
+                fontSize: "10px", // Smaller font size
+                padding: "3px 8px", // Smaller padding
+                border: "1px solid #dc3545", // Red border for Live link
+                borderRadius: "5px",
+                textDecoration: "none",
+                transition: "all 0.3s ease",
+              }}
+              onMouseEnter={(e) => {
+                e.target.style.color = "#fff";
+                e.target.style.backgroundColor = "red";
+                e.target.style.transform = "scale(1.1)";
+              }}
+              onMouseLeave={(e) => {
+                e.target.style.color = "#fff";
+                e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+                e.target.style.transform = "scale(1)";
+              }}
+            >
+              <i
+                className="fa fa-line-chart"
+                aria-hidden="true"
+                style={{ marginRight: "5px" }}
+              ></i>
+              Live
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,143 +5,198 @@ import ImgItem from "../components/ImgItem";
 import VideoItem from "../components/VideoItem";
 
 export default function Portfolio() {
-    const [portfolio, setPortfolio] = useState([]);
-    const [drawing, setDrawing] = useState([]);
-    const [sketch, setSketch] = useState([]);
-    const [dance, setDance] = useState([]);
-    const [certificate, setCertificate] = useState([]);
-    const location = useLocation();
-    useEffect(() => {
-        if (location.pathname === "/portfolio") {
-            // fetch data from backend
-            const data = [
-                { img: "images/portfolio.jpg", url: "#project" },
-                { img: "images/portfolio.jpg", url: "#project" },
-                { img: "images/portfolio.jpg", url: "#project" },
-                { img: "images/portfolio.jpg", url: "#project" },
-                { img: "images/portfolio.jpg", url: "#project" },
-                { img: "images/portfolio.jpg", url: "#project" },
-            ]
-            setPortfolio(data);
-        }
-
-        else if (location.pathname === "/drawing") {
-            //fetch data from backend
-            const data = [
-                { img: "images/drawing/drawing (3).jpg" },
-                { img: "images/drawing/drawing (10).jpg" },
-                { img: "images/drawing/drawing (6).jpg" },
-                { img: "images/drawing/drawing (7).jpg" },
-                { img: "images/drawing/drawing (2).jpg" },
-                { img: "images/drawing/drawing (4).jpg" },
-                { img: "images/drawing/drawing (5).jpg" },
-                { img: "images/drawing/drawing (8).jpg" },
-                { img: "images/drawing/drawing (9).jpg" },
-                
-            ]
-            setDrawing(data);
-        }
-
-        else if (location.pathname === "/sketch") {
-            //fetch data from backend
-            const data = [
-                { img: "images/sketch/sketch (10).jpg" },
-                { img: "images/sketch/sketch (1).jpg" },
-                { img: "images/sketch/sketch (13).jpg" },
-                { img: "images/sketch/sketch (11).jpg" },
-                { img: "images/sketch/sketch (2).jpg" },
-                { img: "images/sketch/sketch (3).jpg" },
-                { img: "images/sketch/sketch (4).jpg" },
-                { img: "images/sketch/sketch (6).jpg" },
-                { img: "images/sketch/sketch (7).jpg" },
-                { img: "images/sketch/sketch (8).jpg" },
-                { img: "images/sketch/sketch (12).jpg" },
-                { img: "images/sketch/sketch (16).jpg" },
-                { img: "images/sketch/sketch (5).jpg" },
-                { img: "images/sketch/sketch (17).jpg" },
-                { img: "images/sketch/sketch (18).jpg" },
-                { img: "images/sketch/sketch (9).jpg" },
-            ]
-            setSketch(data);
-        }
-        else if (location.pathname === "/dance") {
-            //fetch data from backend
-            const data = [
-                { videoUrl: "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514576/portfolio/dance_-_Trim2_mixr8p.mp4" },
-                { videoUrl: "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514534/portfolio/km_20220711_1080p_bcasqb.3gp" },
-                { videoUrl: "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514523/portfolio/ins_1696513335640_x6og10.mp4" },
-                { videoUrl: "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514499/portfolio/dance_-_Trim_zh0ny3.mp4" },
-            ]
-            setDance(data);
-        }
-        else if (location.pathname === "/certificate") {
-            //fetch data from backend
-            const data = [
-                { img: "images/certificate/certificate (4).jpg" },
-                { img: "images/certificate/certificate (2).jpg" },
-                { img: "images/certificate/certificate (3).jpg" },
-                { img: "images/certificate/certificate (8).jpg" },
-                { img: "images/certificate/certificate (1).jpg" },
-                { img: "images/certificate/certificate (9).jpg" },
-                { img: "images/certificate/certificate (5).jpg" },
-                { img: "images/certificate/certificate (6).jpg" },
-                { img: "images/certificate/certificate (7).jpg" },
-            ]
-            setCertificate(data);
-        }
-    }, [location])
-    return (
-        <section className="portfolio section" id="portfolio">
-            {/* hidden */}
-            <div className="container"> 
-
-                <div className="row">
-                    <div className="section-title padd-15">
-                        <h2>
-                            {location.pathname === "/portfolio" && "Portfolio"}
-                            {location.pathname === "/drawing" && "Drawing"}
-                            {location.pathname === "/sketch" && "Sketch"}
-                            {location.pathname === "/dance" && "Dance"}
-                            {location.pathname === "/certificate" && "Certificate"}
-                        </h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="portfolio-heading padd-15">
-                        <h2>My &nbsp;
-                            {location.pathname === "/portfolio" && "Last Project Padding to add"}
-                            {location.pathname === "/drawing" && "Drawing"}
-                            {location.pathname === "/sketch" && "Sketch"}
-                            {location.pathname === "/dance" && "Dance"}
-                            {location.pathname === "/certificate" && "Certificate"}
-                            &nbsp; :</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    {
-                        location.pathname === "/portfolio" ? portfolio.map((item) => {
-                            return (<PortfolioItem item={item} />)
-                        }) :
-
-                            location.pathname === "/drawing" ? drawing.map((item) => {
-                                return (<ImgItem item={item} />)
-                            }) :
-
-                                location.pathname === "/sketch" ? sketch.map((item) => {
-                                    return (<ImgItem item={item} />)
-                                }) :
-
-                                    location.pathname === "/certificate" ? certificate.map((item) => {
-                                        return (<ImgItem item={item} />)
-                                    }) :
-                                        location.pathname === "/dance" && dance.map((item) => {
-                                            return (<VideoItem item={item} />)
-                                        })
-                    }
-
-
-                </div>
-            </div>
-        </section>
-    )
+  const [portfolio, setPortfolio] = useState([]);
+  const [drawing, setDrawing] = useState([]);
+  const [sketch, setSketch] = useState([]);
+  const [dance, setDance] = useState([]);
+  const [certificate, setCertificate] = useState([]);
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/portfolio") {
+      // fetch data from backend
+      const data = [
+        {
+          img: "images/portfolio/shadi.png",
+          githubUrl: "https://github.com/Takeshwar06/Shaadi-Image-Hub-ui-ux",
+          liveUrl: "https://shaadi-image-hub.onrender.com",
+        },
+        {
+          img: "images/portfolio/ecommerce.png",
+          githubUrl: "https://github.com/Takeshwar06/Ecommerce",
+          liveUrl: "https://myecommerce-kohl.vercel.app",
+        },
+        {
+          img: "images/portfolio/myportfolio.png",
+          githubUrl: "https://github.com/Takeshwar06/my-portfolio-frontend",
+          liveUrl: "https://takeshwarcrafts.onrender.com",
+        },
+        {
+          img: "images/portfolio/chatcraze.png",
+          githubUrl: "https://github.com/Takeshwar06/chat-app-frontend",
+          liveUrl: "https://chatcraze.onrender.com",
+        },
+        {
+          img: "images/portfolio/grozzoweb.png",
+          githubUrl: "https://github.com/Takeshwar06",
+          liveUrl: "https://grozzo-admin-frontend.vercel.app",
+        },
+        {
+          img: "images/portfolio/ijsrgi.png",
+          githubUrl: "https://github.com/Takeshwar06/College-Journal-Web",
+          liveUrl: "https://ijsrgi.com",
+        },
+        {
+          img: "images/portfolio/mybillingsystem.png",
+          githubUrl: "https://github.com/Takeshwar06/billing-system",
+          liveUrl: "https://billingsystem-seven.vercel.app",
+        },
+        {
+          img: "images/portfolio/skillspark.png",
+          githubUrl: "https://github.com/Takeshwar06/projectUniverse-frontend",
+          liveUrl: "https://skillspark.onrender.com",
+        },
+        {
+          img: "images/portfolio/smartcanteenweb.png",
+          githubUrl: "https://github.com/Takeshwar06/canteen-web",
+          liveUrl: "https://smartcanteen.onrender.com",
+        },
+        {
+          img: "images/portfolio/smartcanteenapp.png",
+          githubUrl: "https://github.com/Takeshwar06/canteen-native-app",
+          liveUrl: "/apk/smartcanteen.apk",
+          download: true,
+        },
+        {
+          img: "images/portfolio/grozzoapp.png",
+          githubUrl: "https://github.com/Takeshwar06/canteen-web",
+          liveUrl: "/apk/grozzo.apk",
+          download: true,
+        },
+      ];
+      setPortfolio(data);
+    } else if (location.pathname === "/drawing") {
+      //fetch data from backend
+      const data = [
+        { img: "images/drawing/drawing (3).jpg" },
+        { img: "images/drawing/drawing (10).jpg" },
+        { img: "images/drawing/drawing (6).jpg" },
+        { img: "images/drawing/drawing (7).jpg" },
+        { img: "images/drawing/drawing (2).jpg" },
+        { img: "images/drawing/drawing (4).jpg" },
+        { img: "images/drawing/drawing (5).jpg" },
+        { img: "images/drawing/drawing (8).jpg" },
+        { img: "images/drawing/drawing (9).jpg" },
+      ];
+      setDrawing(data);
+    } else if (location.pathname === "/sketch") {
+      //fetch data from backend
+      const data = [
+        { img: "images/sketch/sketch (10).jpg" },
+        { img: "images/sketch/sketch (1).jpg" },
+        { img: "images/sketch/sketch (13).jpg" },
+        { img: "images/sketch/sketch (11).jpg" },
+        { img: "images/sketch/sketch (2).jpg" },
+        { img: "images/sketch/sketch (3).jpg" },
+        { img: "images/sketch/sketch (4).jpg" },
+        { img: "images/sketch/sketch (6).jpg" },
+        { img: "images/sketch/sketch (7).jpg" },
+        { img: "images/sketch/sketch (8).jpg" },
+        { img: "images/sketch/sketch (12).jpg" },
+        { img: "images/sketch/sketch (16).jpg" },
+        { img: "images/sketch/sketch (5).jpg" },
+        { img: "images/sketch/sketch (17).jpg" },
+        { img: "images/sketch/sketch (18).jpg" },
+        { img: "images/sketch/sketch (9).jpg" },
+      ];
+      setSketch(data);
+    } else if (location.pathname === "/dance") {
+      //fetch data from backend
+      const data = [
+        {
+          videoUrl:
+            "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514576/portfolio/dance_-_Trim2_mixr8p.mp4",
+        },
+        {
+          videoUrl:
+            "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514534/portfolio/km_20220711_1080p_bcasqb.3gp",
+        },
+        {
+          videoUrl:
+            "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514523/portfolio/ins_1696513335640_x6og10.mp4",
+        },
+        {
+          videoUrl:
+            "https://res.cloudinary.com/do3fiil0d/video/upload/v1696514499/portfolio/dance_-_Trim_zh0ny3.mp4",
+        },
+      ];
+      setDance(data);
+    } else if (location.pathname === "/certificate") {
+      //fetch data from backend
+      const data = [
+        { img: "images/certificate/certificate (4).jpg" },
+        { img: "images/certificate/certificate (2).jpg" },
+        { img: "images/certificate/certificate (3).jpg" },
+        { img: "images/certificate/certificate (8).jpg" },
+        { img: "images/certificate/certificate (1).jpg" },
+        { img: "images/certificate/certificate (9).jpg" },
+        { img: "images/certificate/certificate (5).jpg" },
+        { img: "images/certificate/certificate (6).jpg" },
+        { img: "images/certificate/certificate (7).jpg" },
+      ];
+      setCertificate(data);
+    }
+  }, [location]);
+  return (
+    <section className="portfolio section" id="portfolio">
+      {/* hidden */}
+      <div className="container">
+        <div className="row">
+          <div className="section-title padd-15">
+            <h2>
+              {location.pathname === "/portfolio" && "Portfolio"}
+              {location.pathname === "/drawing" && "Drawing"}
+              {location.pathname === "/sketch" && "Sketch"}
+              {location.pathname === "/dance" && "Dance"}
+              {location.pathname === "/certificate" && "Certificate"}
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="portfolio-heading padd-15">
+            <h2>
+              My &nbsp;
+              {location.pathname === "/portfolio" && "Last Project"}
+              {location.pathname === "/drawing" && "Drawing"}
+              {location.pathname === "/sketch" && "Sketch"}
+              {location.pathname === "/dance" && "Dance"}
+              {location.pathname === "/certificate" && "Certificate"}
+              &nbsp; :
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          {location.pathname === "/portfolio"
+            ? portfolio.map((item) => {
+                return <PortfolioItem item={item} />;
+              })
+            : location.pathname === "/drawing"
+            ? drawing.map((item) => {
+                return <ImgItem item={item} />;
+              })
+            : location.pathname === "/sketch"
+            ? sketch.map((item) => {
+                return <ImgItem item={item} />;
+              })
+            : location.pathname === "/certificate"
+            ? certificate.map((item) => {
+                return <ImgItem item={item} />;
+              })
+            : location.pathname === "/dance" &&
+              dance.map((item) => {
+                return <VideoItem item={item} />;
+              })}
+        </div>
+      </div>
+    </section>
+  );
 }
